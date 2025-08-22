6e0984e5ad10cab7f838a6a5d488e3d286d3a7fb
Satnam <satnamsingh@vancedsolutions.com>
2025-08-22 18:45:45 +0530

    add

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = require('../src/api/routes')
-
+const cors = require("cors"); 
 const app = express();
+
+
+app.use(
+  cors({
+    origin: "*", 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 app.use(router);
--- src/server.js
+++ src/server.js
@@ -2,10 +2,9 @@ require('dotenv').config();
 const app = require('./app');
 const connectDB = require('../src/config/db');
 
-// Connect DB
+
 connectDB();
 
-// Start server
 const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
