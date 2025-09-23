985fb2ba01fc799cfcaa24bfff2cc224917ad077
satnamsinghvanced <145443805+satnamsinghvanced@users.noreply.github.com>
2025-09-23 15:12:55 +0530

    Create auth api's

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import userRouter from "./usersRoute.ts";
 import subscriptionRoute from "./subscriptionRoute.ts";
+
 const router = express.Router();
 
+router.use("/users",userRouter)
 router.use("/subscriptions", subscriptionRoute);
 
 export default router;
--- src/routes/usersRoute.ts
+++ src/routes/usersRoute.ts
@@ -1,8 +1,16 @@
 import express from "express";
+import userController from "../controllers/users/index.ts";
+import auth from "../utils/middleware.ts";
 
-const router = express.Router();
+const userRouter = express.Router();
 
+userRouter.post("/signup", userController.signup);
+userRouter.post("/login", userController.login);
 
-export default router
+userRouter.get("/all", auth, userController.getAllUser);
+userRouter.get("/user", auth, userController.getUserById);
+userRouter.put("/user", auth, userController.updateUser);
+userRouter.delete("/user", auth, userController.deleteUser);
 
-
+export default userRouter;
+ 
